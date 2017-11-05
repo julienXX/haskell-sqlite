4391838b0af6b87f324aa2265167fa0e074ab6ab
Julien Blanchard <julien@sideburns.eu>
2017-11-05 14:34:56 +0100

    Parse select, insert and meta commands

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -4,6 +4,8 @@ module Interpreter
       console
       -- * Interpreter
     , Command(..), interpret
+      -- * Helpers
+    , queryStartsWithSelect, queryStartsWithInsert, queryStartsWithDot
     )
 where
 
@@ -11,12 +13,28 @@ import Data.Text
 import Data.Monoid((<>))
 
 data Command = Exit
+             | Select
+             | Insert
+             | UnknownMeta Text
              | Unknown Text
   deriving (Eq, Show)
 
 interpret :: Text -> Command
-interpret ".exit" = Exit
-interpret unknown = Unknown unknown
+interpret ".exit"  = Exit
+interpret query
+  | queryStartsWithSelect query = Select
+  | queryStartsWithInsert query = Insert
+  | queryStartsWithDot    query = UnknownMeta query
+interpret unknown  = Unknown unknown
+
+queryStartsWithSelect :: Text -> Bool
+queryStartsWithSelect query = "select" `isPrefixOf` query
+
+queryStartsWithInsert :: Text -> Bool
+queryStartsWithInsert query = "insert" `isPrefixOf` query
+
+queryStartsWithDot :: Text -> Bool
+queryStartsWithDot query = Data.Text.head query == '.'
 
 console :: IO ()
 console = do
@@ -24,7 +42,16 @@ console = do
   line <- getLine
   let output = interpret $ pack line
   case output of
-    Exit        -> putStrLn "bye !"
+    Exit -> putStrLn "bye !"
+    Select -> do
+      putStrLn "This is where we would do a select."
+      console
+    Insert -> do
+      putStrLn "This is where we would do an insert."
+      console
+    UnknownMeta meta -> do
+      putStrLn ("Unrecognized command: " <> unpack meta)
+      console
     Unknown com -> do
-      putStrLn ("Unknown command : " <> unpack com)
+      putStrLn ("Unrecognized keyword at start of: " <> unpack com)
       console
--- test/ConsoleSpec.hs
+++ test/ConsoleSpec.hs
@@ -8,5 +8,29 @@ spec :: Spec
 spec = describe "SQL Mini Interpreter" $ do
   it "interprets '.exit' as Exit command" $ do
     interpret ".exit" `shouldBe` Exit
+  it "interprets 'select' as Select command" $ do
+    interpret "select" `shouldBe` Select
+  it "interprets 'select from foo' as Select command" $ do
+    interpret "select from foo" `shouldBe` Select
+  it "interprets 'insert' as Insert command" $ do
+    interpret "insert" `shouldBe` Insert
+  it "interprets 'insert foo' as Insert command" $ do
+    interpret "insert foo" `shouldBe` Insert
+  it "interprets unknown meta command as UnknownMeta command" $ do
+    interpret ".foo" `shouldBe` UnknownMeta ".foo"
   it "interprets unknown string  as Unknown command" $ do
     interpret "foo" `shouldBe` Unknown "foo"
+
+  describe "Parsing helpers" $ do
+    it "returns if a query starts with 'select'" $ do
+      queryStartsWithSelect "select foo" `shouldBe` True
+    it "returns if a query starts with 'select'" $ do
+      queryStartsWithSelect "foo" `shouldBe` False
+    it "returns if a query starts with 'insert'" $ do
+      queryStartsWithInsert "insert foo" `shouldBe` True
+    it "returns if a query starts with 'insert'" $ do
+      queryStartsWithInsert "foo" `shouldBe` False
+    it "returns if a query starts with a dot" $ do
+      queryStartsWithDot ".tables" `shouldBe` True
+    it "returns if a query starts with a dot" $ do
+      queryStartsWithDot "tables" `shouldBe` False
